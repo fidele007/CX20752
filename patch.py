dc697d9005c6c2ad7767ca76e65e4d37d4634b42
Force Fidele KIEN <forcefidele.kien@parispartners.com>
2018-11-22 16:46:53 +0100

    Add compatibility with Python 2

--- patch.py
+++ patch.py
@@ -10,34 +10,34 @@ def patch_codec_commander(kext_path):
         return
 
     with open(info_path, 'rb') as fp:
-        plist = plistlib.load(fp)
+        plist = plistlib.readPlist(fp) if sys.version_info[0] < 3 else plistlib.load(fp)
 
-    # Add profile for CX20752
-    plist['IOKitPersonalities']['CodecCommander']['Codec Profile']['14f1_510f'] = 'CX20752'
+        # Add profile for CX20752
+        plist['IOKitPersonalities']['CodecCommander']['Codec Profile']['14f1_510f'] = 'CX20752'
 
-    # Configure CX20752 profile
-    custom_command_1 = {'Command': '0x01970724',
-                        'Comment': '0x19 SET_PIN_WIDGET_CONTROL 0x24',
-                        'On Init': True,
-                        'On Sleep': False,
-                        'On Wake': True
-                       }
-    custom_command_2 = {'Command': '0x01a70724',
-                        'Comment': '0x1a SET_PIN_WIDGET_CONTROL 0x24',
-                        'On Init': True,
-                        'On Sleep': False,
-                        'On Wake': True
-                       }
-    conexant_dict = {'Custom Commands': [custom_command_1, custom_command_2],
-                     'Perform Reset': False,
-                     'Perform Reset on External Wake': False
-                    }
-    plist['IOKitPersonalities']['CodecCommander']['Codec Profile']['CX20752'] = conexant_dict
+        # Configure CX20752 profile
+        custom_command_1 = {'Command': '0x01970724',
+                            'Comment': '0x19 SET_PIN_WIDGET_CONTROL 0x24',
+                            'On Init': True,
+                            'On Sleep': False,
+                            'On Wake': True
+                           }
+        custom_command_2 = {'Command': '0x01a70724',
+                            'Comment': '0x1a SET_PIN_WIDGET_CONTROL 0x24',
+                            'On Init': True,
+                            'On Sleep': False,
+                            'On Wake': True
+                           }
+        conexant_dict = {'Custom Commands': [custom_command_1, custom_command_2],
+                         'Perform Reset': False,
+                         'Perform Reset on External Wake': False
+                        }
+        plist['IOKitPersonalities']['CodecCommander']['Codec Profile']['CX20752'] = conexant_dict
 
-    with open(info_path, 'wb') as fp:
-        plistlib.dump(plist, fp)
+        with open(info_path, 'wb') as fp:
+            plistlib.writePlist(plist, fp) if sys.version_info[0] < 3 else plistlib.dump(plist, fp)
 
-    print('Patch done.')
+        print('Patch done.')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
